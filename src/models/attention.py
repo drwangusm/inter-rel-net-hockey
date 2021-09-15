97d973b65f3ba4d15bf4a9875cb4099f9604624a
Farzaneh Askari <farzaneh.askari@mail.mcgill.ca>
2021-09-15 11:18:09 -0400

    transformer self-attention

--- src/models/attention.py
+++ src/models/attention.py
@@ -1,9 +1,13 @@
+from __future__ import print_function
 import keras
 import keras.backend as K
 import tensorflow as tf
+from keras.layers import Dropout
+
 import tensorflow.nn as nn
 import numpy as np
 
+'''
 #first version of attention model
 
 class IRNAttention(keras.layers.Layer):
@@ -47,6 +51,74 @@ class IRNAttention(keras.layers.Layer):
                        "projection_size": self.projection_size,
                        "return_attention": self.return_attention})
         return config
+
+
+'''
+
+#this is transformer attention
+
+class IRNAttention(keras.layers.Layer):
+    def __init__(self, num_head=1, projection_size=None, return_attention=False, motion=None, **kwargs):
+        self.return_attention = return_attention
+        self.projection_size = projection_size
+        self.num_head = num_head
+        # self.motion = motion
+
+        super(IRNAttention, self).__init__(**kwargs)
+
+    def build(self, input_size):
+        if self.projection_size is None:
+            # self.projection_size = input_size[0][1] # size of joint object
+            self.projection_size = 500
+
+        self.w1=self.add_weight(name="Att1", shape=(input_size[0][1], self.projection_size), initializer="normal") #name property is useful for avoiding RuntimeError: Unable to create link.
+        self.w2=self.add_weight(name="Att2", shape=(input_size[0][1], self.projection_size), initializer="normal")
+        # self.w3=self.add_weight(name="Att3", shape=(input_size[0][1], self.projection_size), initializer="normal") #name property is useful for avoiding RuntimeError: Unable to create link.
+        self.w4=self.add_weight(name="Att4", shape=(self.projection_size, 1), initializer="normal")
+
+        super(IRNAttention, self).build(input_size)
+
+    def call(self, inputs):
+
+        inputs = tf.transpose(tf.stack(inputs), perm=[1,0,2])
+        query_n = inputs * 1
+        key_n = inputs + 0
+        values = inputs / 1
+
+        query = K.tanh(K.dot(query_n,self.w1))
+        key = K.tanh(K.dot(key_n,self.w2))
+        temper = tf.sqrt(tf.cast(self.projection_size, dtype='float32'))
+        attn1 = tf.matmul(query, tf.transpose(key, perm=[0,2,1])) /temper
+        attn_nod = K.softmax(attn1, axis=2)
+        attn = Dropout(0.1)(attn_nod)
+        output = tf.matmul(attn, values)
+        # sentence = K.sum(output, axis=1)
+
+        #here apply the self-attention mechansim we had before
+        # e = K.tanh(K.dot(output,self.w3))
+        #e2 = K.dot(e, self.w4)
+        e2 = K.tanh(K.dot(output,self.w4))
+        a = K.softmax(e2, axis=1)
+        new_output = inputs*a
+        ##return the ouputs. 'a' is the set of attention weights
+        ##the second variable is the 'attention adjusted o/p state' or context
+        sentence = K.sum(new_output, axis=1)
+        attention = a
+
+
+        if self.return_attention:
+            return [sentence, attn]
+        else:
+            return (sentence)
+
+    def get_config(self):
+        config = super(IRNAttention, self).get_config()
+        config.update({"num_head": self.num_head,
+                       "projection_size": self.projection_size,
+                       "return_attention": self.return_attention})
+        return config
+    
+
 
 '''
 #this is the second version of self-attention ==> doesn't work well
@@ -98,4 +170,51 @@ class IRNAttention(keras.layers.Layer):
                        "return_attention": self.return_attention})
         return config
     
+'''
+'''
+class IRNAttention(keras.layers.Layer):
+    def __init__(self, num_head=1, projection_size=None, return_attention=False, motion=None, **kwargs):
+        self.return_attention = return_attention
+        self.projection_size = projection_size
+        self.num_head = num_head
+        self.motion = motion
+        super(IRNAttention, self).__init__(**kwargs)
+
+    def build(self, input_size):
+        # if self.projection_size is None:
+        #     self.projection_size = input_size[0][1] # size of joint object
+        #
+        # #todo add another layer
+        # #todo add bias
+        #
+        # self.w1=self.add_weight(name="Att1", shape=(input_size[0][1], self.projection_size), initializer="normal") #name property is useful for avoiding RuntimeError: Unable to create link.
+        # self.w2=self.add_weight(name="Att2", shape=(self.projection_size, 1), initializer="normal")
+
+        super(IRNAttention, self).build(input_size)
+
+    def call(self, inputs):
+
+        inputs = tf.transpose(tf.stack(inputs), perm=[1,0,2])
+        motionm = tf.transpose(tf.stack(self.motion), perm=[1,0,2])
+        # motionm = motionm / motionm
+        a = K.softmax(motionm, axis=1)
+        # a = tf.Print(a, [a])
+        output = inputs*a
+        # ##return the ouputs. 'a' is the set of attention weights
+        # ##the second variable is the 'attention adjusted o/p state' or context
+        sentence = K.sum(output, axis=1)
+        attention = a
+
+        if self.return_attention:
+            return [sentence, attention]
+        else:
+            return (sentence)
+
+    def get_config(self):
+        config = super(IRNAttention, self).get_config()
+        config.update({"num_head": self.num_head,
+                       "projection_size": self.projection_size,
+                       "return_attention": self.return_attention})
+        return config
+
 '''
