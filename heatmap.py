7ed683c0230c8cbe447c1c63830d44d2b481f320
drwangusm <997459445@qq.com>
2024-04-14 13:48:18 +0800

    add heatmap

--- heatmap.py
+++ heatmap.py
@@ -1,20 +1,61 @@
-import seaborn as sns
 import pandas as pd
 import matplotlib.pyplot as plt
-plt.rcParams['font.family'] = 'Times New Roman' # 设置英文字体为Times New Roman
-df = pd.read_csv('final_results.csv')#这里的data.csv为任意csv文件
-df_corr = df.corr()#计算相关性系数
-plt.figure(figsize=(10,8))#figsize可以规定热力图大小
-plt.xticks(size=18,fontproperties='Times New Roman',weight='bold')
-plt.yticks(size=18,fontproperties='Times New Roman',weight='bold')
-fig = sns.heatmap(df_corr, #所绘数据
-                  cmap='coolwarm', #颜色
-                annot=True,fmt='.3g',#annot为热力图上显示数据；结果保留3位数字
-                annot_kws={'size': 18, 'style': 'normal', #字体大小和格式
-                           'family':'Times New Roman','weight': 'bold'}, #字体和风格--加粗
-                linewidths=3, #图框分割线宽度
-                square=True,  #使每个图框大小一致
-                cbar = True) #绘制图例
-cbar = fig.collections[0].colorbar
-cbar.ax.tick_params(labelsize=20)  #设置图例字体大小
-fig
+
+# 加载数据
+data = pd.read_csv('final_results.csv')
+
+# 提取模型名称和评估指标数据
+models = data['model']
+accuracy = data['val_acc_mean']
+recall = data['val_recall_mean']
+precision = data['val_precision_mean']
+f1 = data['val_f1_mean']
+
+# 绘制柱状图
+plt.figure(figsize=(16, 12))
+plt.bar(models, accuracy, label='Accuracy')
+plt.bar(models, recall, label='Recall')
+plt.bar(models, precision, label='Precision')
+plt.bar(models, f1, label='F1')
+plt.xlabel('Model')
+plt.ylabel('Score')
+plt.title('Evaluation Metrics for Different Models')
+plt.xticks(rotation=90)
+plt.legend()
+plt.savefig("hist.png")
+plt.show()
+
+# 绘制折线图
+plt.figure(figsize=(16, 12))
+plt.plot(models, accuracy, marker='o', label='Accuracy')
+plt.plot(models, recall, marker='o', label='Recall')
+plt.plot(models, precision, marker='o', label='Precision')
+plt.plot(models, f1, marker='o', label='F1')
+plt.xlabel('Model')
+plt.ylabel('Score')
+plt.title('Evaluation Metrics for Different Models')
+plt.xticks(rotation=90)
+plt.legend()
+plt.grid(True)
+plt.savefig("pr.png")
+plt.show()
+
+import seaborn as sns
+
+# 设置索引为模型名称
+data.set_index('model', inplace=True)
+
+# 绘制热力图
+plt.figure(figsize=(16, 12))
+sns.heatmap(data, annot=True, cmap='YlGnBu', fmt=".2f")
+plt.title('Evaluation Metrics for Different Models')
+plt.xlabel('Metrics')
+plt.ylabel('Model')
+plt.xticks(rotation=45)
+plt.yticks(rotation=0)
+plt.savefig("matrix.png")
+plt.show()
+
+
+
+
---
+++ summary_stats.sh
@@ -0,0 +1,5 @@
+#查看模型训练日志状态，执行命令例子
+python src/misc/print_train_stats.py /usr/local/inter-rel-net-hockey/data01/models/UT-1/* -c val_loss
+
+#运行summzrizeResults.py
+python summarizeResults.py /usr/local/inter-rel-net-hockey/data01/models/UT-1
